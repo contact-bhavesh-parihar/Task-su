f4e5f522d6fce4154f9db425bb4e12285463d62f
Bhavii <contact.bhavesh.parihar@gmail.com>
2026-05-21 16:26:42 +0530

    Fix import path for User model in auth.py

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
 
-from backend.model import db, User
+from ..model import db, User
 
 auth_bp = Blueprint('auth', __name__)
 
