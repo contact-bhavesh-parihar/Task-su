173d8ae278d3e6ff666d4e9086091907fae7bf16
Bhavii <contact.bhavesh.parihar@gmail.com>
2026-05-21 16:34:37 +0530

    Update task.py

--- backend/routes/task.py
+++ backend/routes/task.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from ..model import db, Task, ProjectMember
+from model import db, Task, ProjectMember
 
 tasks_bp = Blueprint('tasks', __name__)
 
